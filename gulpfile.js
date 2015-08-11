505d9b2a5897a98ce284ad26488d1b70b20f9a9b
Joe Hare <joseph.hare@gmail.com>
2015-08-11 16:56:31 -0500

    Create really dumb API handler.

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+'use strict';
+var gulp = require('gulp');
+
+gulp.task('test', function thereAreNoTests() {
+  console.log('Sorry didn\'t bother with tests on this one. It works.');
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+var apiHandler = require('../lib/api-handler');
+
+module.exports = apiHandler;
